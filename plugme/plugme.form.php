282b83ba6d9206b1e6655f18acb429b5d7b4d167
Francois Lajoie <snake386@hotmail.com>
2016-08-14 00:37:25 -0400

    fixed description and image.added form slider and country list

--- plugme/plugme.form.php
+++ plugme/plugme.form.php
@@ -258,6 +258,8 @@ abstract class plugme_form
     {
         if(!empty($this->form_fields)) {
 
+            $first_field = '';
+
             // form buttons
             echo '
                 <div class="alignright">
@@ -270,6 +272,8 @@ abstract class plugme_form
 
             foreach($this->form_fields as $k => $v) {
 
+                if(empty($first_field)) $first_field = $k;
+
                 if(!array_key_exists('type', $v)) $v['type'] = 'text';
                 $method = 'form_'.$v['type'];
 
@@ -277,11 +281,13 @@ abstract class plugme_form
 
                 $description = (array_key_exists('description', $v)) ? $v['description'] : '';
 
+                if($v['type'] != 'switch') $description = '<br>'.$description;
+
                 echo '<tr>
                         <th scope="row">
                             <label for="field-'.$k.'">'.__($v['label']).'</label>
                         </th>
-                        <td>'.$component.'<br><small>'.$description.'</small></td>
+                        <td>'.$component.'<small>'.$description.'</small></td>
                     </tr>'; 
             }
 
@@ -299,6 +305,16 @@ abstract class plugme_form
                     <a href="?page='.$_REQUEST['page'].'" class="button">'.__('Cancel').'</a> &nbsp;
                     <input type="submit" value="'.__('Save').'" class="button button-primary" id="submit" name="submit">
                 </div>';
+
+
+            // script (auto focus)
+            echo '
+                <script>
+                jQuery(function() {
+                    jQuery("#field-'.$first_field.'").focus();
+                });
+                </script>
+            ';
            
         }
     }
@@ -443,13 +459,20 @@ abstract class plugme_form
      */
     public function form_image($name, $data, $options = null)
     {
+        if(empty($data)) $no_image = true;
+
         $id_name = 'field-'.$name;
         $image_url = site_url().'/'.$data;
         $control = '
             <input type="text" id="'.$id_name.'" name="'.$name.'" class="regular-text" value="'.$data.'">
-            <input type="button" name="upload-btn" id="upload-btn-'.$id_name.'" class="button-secondary" value="'.__('Upload image').'">
-            <br><img id="image-'.$id_name.'" src="'.$image_url .'" width="300px">';
+            <input type="button" name="upload-btn" id="upload-btn-'.$id_name.'" class="button-secondary" value="'.__('Upload image').'">';
 
+        if(!isset($no_image)) {
+            $control .= '<br><img id="image-'.$id_name.'" src="'.$image_url .'" width="300px">';
+        }
+        else {
+            $control .= '<br><img id="image-'.$id_name.'" src="'.$image_url .'" width="0px">';
+        }         
 
         $control .= '
             <script type="text/javascript">
@@ -473,8 +496,10 @@ abstract class plugme_form
                             var real_image_url = image_url.replace("images/", "'.site_url().'/wp-content/uploads/");
                             // Let"s assign the url value to the input field
                             $("#'.$id_name.'").val(image_url.replace("'.site_url().'", ""));
-                            $("#image-'.$id_name.'").attr("src", real_image_url);
-
+                            $("#image-'.$id_name.'").attr({
+                                "src" : real_image_url,
+                                "width" : "300px"
+                            });
                         });
                     });
                 });
@@ -499,6 +524,27 @@ abstract class plugme_form
             <script>
                 jQuery(document).ready(function(){
                     jQuery("#'.$id_name.'").datepicker({ dateFormat: "'.$format.'" }); 
+                });
+            </script>';
+
+        return $control;
+    }
+
+    /**
+     * Slider
+     *
+     * @uses  jquery-ui
+     */
+    public function form_slider($name, $data, $options = null)
+    {
+        $id_name = 'field-'.$name;
+        //$format = (array_key_exists('format', $options)) ? $options['format'] : 'yy-mm-dd';
+        $control = '
+            <div id="slider-'.$id_name.'" style="max-width:400px;"></div>
+            <input type="hidden" id="'.$id_name.'" name="'.$name.'" value="'.$data.'" />
+            <script>
+                jQuery(document).ready(function(){
+                    jQuery("#slider-'.$id_name.'").slider(); 
                 });
             </script>';
 
@@ -551,6 +597,257 @@ abstract class plugme_form
         return $control;
     }
     
-
+    public function get_country_list()
+    {
+        return array(
+            'AF' => "Afghanistan",
+            'AL' => "Albania",
+            'DZ' => "Algeria",
+            'AS' => "American Samoa",
+            'AD' => "Andorra",
+            'AG' => "Angola",
+            'AI' => "Anguilla",
+            'AG' => "Antigua &amp; Barbuda",
+            'AR' => "Argentina",
+            'AA' => "Armenia",
+            'AW' => "Aruba",
+            'AU' => "Australia",
+            'AT' => "Austria",
+            'AZ' => "Azerbaijan",
+            'BS' => "Bahamas",
+            'BH' => "Bahrain",
+            'BD' => "Bangladesh",
+            'BB' => "Barbados",
+            'BY' => "Belarus",
+            'BE' => "Belgium",
+            'BZ' => "Belize",
+            'BJ' => "Benin",
+            'BM' => "Bermuda",
+            'BT' => "Bhutan",
+            'BO' => "Bolivia",
+            'BL' => "Bonaire",
+            'BA' => "Bosnia &amp; Herzegovina",
+            'BW' => "Botswana",
+            'BR' => "Brazil",
+            'BC' => "British Indian Ocean Ter",
+            'BN' => "Brunei",
+            'BG' => "Bulgaria",
+            'BF' => "Burkina Faso",
+            'BI' => "Burundi",
+            'KH' => "Cambodia",
+            'CM' => "Cameroon",
+            'CA' => "Canada",
+            'IC' => "Canary Islands",
+            'CV' => "Cape Verde",
+            'KY' => "Cayman Islands",
+            'CF' => "Central African Republic",
+            'TD' => "Chad",
+            'CD' => "Channel Islands",
+            'CL' => "Chile",
+            'CN' => "China",
+            'CI' => "Christmas Island",
+            'CS' => "Cocos Island",
+            'CO' => "Colombia",
+            'CC' => "Comoros",
+            'CG' => "Congo",
+            'CK' => "Cook Islands",
+            'CR' => "Costa Rica",
+            'CT' => "Cote D'Ivoire",
+            'HR' => "Croatia",
+            'CU' => "Cuba",
+            'CB' => "Curacao",
+            'CY' => "Cyprus",
+            'CZ' => "Czech Republic",
+            'DK' => "Denmark",
+            'DJ' => "Djibouti",
+            'DM' => "Dominica",
+            'DO' => "Dominican Republic",
+            'TM' => "East Timor",
+            'EC' => "Ecuador",
+            'EG' => "Egypt",
+            'SV' => "El Salvador",
+            'GQ' => "Equatorial Guinea",
+            'ER' => "Eritrea",
+            'EE' => "Estonia",
+            'ET' => "Ethiopia",
+            'FA' => "Falkland Islands",
+            'FO' => "Faroe Islands",
+            'FJ' => "Fiji",
+            'FI' => "Finland",
+            'FR' => "France",
+            'GF' => "French Guiana",
+            'PF' => "French Polynesia",
+            'FS' => "French Southern Ter",
+            'GA' => "Gabon",
+            'GM' => "Gambia",
+            'GE' => "Georgia",
+            'DE' => "Germany",
+            'GH' => "Ghana",
+            'GI' => "Gibraltar",
+            'GB' => "Great Britain",
+            'GR' => "Greece",
+            'GL' => "Greenland",
+            'GD' => "Grenada",
+            'GP' => "Guadeloupe",
+            'GU' => "Guam",
+            'GT' => "Guatemala",
+            'GN' => "Guinea",
+            'GY' => "Guyana",
+            'HT' => "Haiti",
+            'HW' => "Hawaii",
+            'HN' => "Honduras",
+            'HK' => "Hong Kong",
+            'HU' => "Hungary",
+            'IS' => "Iceland",
+            'IN' => "India",
+            'ID' => "Indonesia",
+            'IA' => "Iran",
+            'IQ' => "Iraq",
+            'IR' => "Ireland",
+            'IM' => "Isle of Man",
+            'IL' => "Israel",
+            'IT' => "Italy",
+            'JM' => "Jamaica",
+            'JP' => "Japan",
+            'JO' => "Jordan",
+            'KZ' => "Kazakhstan",
+            'KE' => "Kenya",
+            'KI' => "Kiribati",
+            'NK' => "Korea North",
+            'KS' => "Korea South",
+            'KW' => "Kuwait",
+            'KG' => "Kyrgyzstan",
+            'LA' => "Laos",
+            'LV' => "Latvia",
+            'LB' => "Lebanon",
+            'LS' => "Lesotho",
+            'LR' => "Liberia",
+            'LY' => "Libya",
+            'LI' => "Liechtenstein",
+            'LT' => "Lithuania",
+            'LU' => "Luxembourg",
+            'MO' => "Macau",
+            'MK' => "Macedonia",
+            'MG' => "Madagascar",
+            'MY' => "Malaysia",
+            'MW' => "Malawi",
+            'MV' => "Maldives",
+            'ML' => "Mali",
+            'MT' => "Malta",
+            'MH' => "Marshall Islands",
+            'MQ' => "Martinique",
+            'MR' => "Mauritania",
+            'MU' => "Mauritius",
+            'ME' => "Mayotte",
+            'MX' => "Mexico",
+            'MI' => "Midway Islands",
+            'MD' => "Moldova",
+            'MC' => "Monaco",
+            'MN' => "Mongolia",
+            'MS' => "Montserrat",
+            'MA' => "Morocco",
+            'MZ' => "Mozambique",
+            'MM' => "Myanmar",
+            'NA' => "Nambia",
+            'NU' => "Nauru",
+            'NP' => "Nepal",
+            'AN' => "Netherland Antilles",
+            'NL' => "Netherlands (Holland, Europe)",
+            'NV' => "Nevis",
+            'NC' => "New Caledonia",
+            'NZ' => "New Zealand",
+            'NI' => "Nicaragua",
+            'NE' => "Niger",
+            'NG' => "Nigeria",
+            'NW' => "Niue",
+            'NF' => "Norfolk Island",
+            'NO' => "Norway",
+            'OM' => "Oman",
+            'PK' => "Pakistan",
+            'PW' => "Palau Island",
+            'PS' => "Palestine",
+            'PA' => "Panama",
+            'PG' => "Papua New Guinea",
+            'PY' => "Paraguay",
+            'PE' => "Peru",
+            'PH' => "Philippines",
+            'PO' => "Pitcairn Island",
+            'PL' => "Poland",
+            'PT' => "Portugal",
+            'PR' => "Puerto Rico",
+            'QA' => "Qatar",
+            'ME' => "Republic of Montenegro",
+            'RS' => "Republic of Serbia",
+            'RE' => "Reunion",
+            'RO' => "Romania",
+            'RU' => "Russia",
+            'RW' => "Rwanda",
+            'NT' => "St Barthelemy",
+            'EU' => "St Eustatius",
+            'HE' => "St Helena",
+            'KN' => "St Kitts-Nevis",
+            'LC' => "St Lucia",
+            'MB' => "St Maarten",
+            'PM' => "St Pierre &amp; Miquelon",
+            'VC' => "St Vincent &amp; Grenadines",
+            'SP' => "Saipan",
+            'SO' => "Samoa",
+            'AS' => "Samoa American",
+            'SM' => "San Marino",
+            'ST' => "Sao Tome &amp; Principe",
+            'SA' => "Saudi Arabia",
+            'SN' => "Senegal",
+            'RS' => "Serbia",
+            'SC' => "Seychelles",
+            'SL' => "Sierra Leone",
+            'SG' => "Singapore",
+            'SK' => "Slovakia",
+            'SI' => "Slovenia",
+            'SB' => "Solomon Islands",
+            'OI' => "Somalia",
+            'ZA' => "South Africa",
+            'ES' => "Spain",
+            'LK' => "Sri Lanka",
+            'SD' => "Sudan",
+            'SR' => "Suriname",
+            'SZ' => "Swaziland",
+            'SE' => "Sweden",
+            'CH' => "Switzerland",
+            'SY' => "Syria",
+            'TA' => "Tahiti",
+            'TW' => "Taiwan",
+            'TJ' => "Tajikistan",
+            'TZ' => "Tanzania",
+            'TH' => "Thailand",
+            'TG' => "Togo",
+            'TK' => "Tokelau",
+            'TO' => "Tonga",
+            'TT' => "Trinidad &amp; Tobago",
+            'TN' => "Tunisia",
+            'TR' => "Turkey",
+            'TU' => "Turkmenistan",
+            'TC' => "Turks &amp; Caicos Is",
+            'TV' => "Tuvalu",
+            'UG' => "Uganda",
+            'UA' => "Ukraine",
+            'AE' => "United Arab Emirates",
+            'GB' => "United Kingdom",
+            'US' => "United States of America",
+            'UY' => "Uruguay",
+            'UZ' => "Uzbekistan",
+            'VU' => "Vanuatu",
+            'VS' => "Vatican City State",
+            'VE' => "Venezuela",
+            'VN' => "Vietnam",
+            'VB' => "Virgin Islands (Brit)",
+            'VA' => "Virgin Islands (USA)",
+            'WK' => "Wake Island",
+            'WF' => "Wallis &amp; Futana Is",
+            'YE' => "Yemen",
+            'ZR' => "Zaire",
+            'ZM' => "Zambia",
+            'ZW' => "Zimbabwe",
+        );
+    }
 
 }

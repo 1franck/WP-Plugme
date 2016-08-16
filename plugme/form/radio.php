8f403666ece2a07e21e6b27c3a54849f2c55c636
Francois Lajoie <snake386@hotmail.com>
2016-08-15 22:20:04 -0400

    added

---
+++ plugme/form/radio.php
@@ -0,0 +1,50 @@
+<?php
+
+/**
+ * HTML Select
+ */
+class plugme_form_control_radio extends plugme_form_control
+{
+    /**
+     * Default options
+     * @var array
+     */
+    protected $default_options = array(
+        'description' => '', 
+        'options'     => array(),
+        'attrs'       => array(
+            'required'    => false,
+        ),
+    );
+
+    /**
+     * Generated the control
+     * 
+     * @return string
+     */
+    public function generate()
+    {
+        $attrs = $this->attributes(false);
+
+        $control = '
+            <fieldset>
+                <legend class="screen-reader-text">
+                    <span>'.$this->options['label'].'</span>
+                </legend>';
+
+
+        if(array_key_exists('options', $this->options) && !empty($this->options['options'])) {
+            foreach($this->options['options'] as $k => $v) {
+                $control .= '
+                    <label>
+                        <input type="radio" '.(($k == $this->data) ? 'checked' : '').' value="'.$k.'" name="'.$this->name.'">
+                        <span>'.$v.'</span>
+                    </label>';
+            }
+        }
+        
+        $control .= '</fieldset><br>';
+
+        return $control;
+    }
+}

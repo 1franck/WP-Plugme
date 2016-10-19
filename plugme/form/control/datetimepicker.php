5d5063436fded381eaef8049cabcdd92b6aad8e0
Francois Lajoie <snake386@hotmail.com>
2016-10-19 14:45:08 -0400

    added some default options

--- plugme/form/control/datetimepicker.php
+++ plugme/form/control/datetimepicker.php
@@ -14,6 +14,16 @@ class plugme_form_control_datetimepicker extends plugme_form_control
         'format'      => 'Y-m-d H:i',
         'description' => '',
         'datepicker'  => true,
+        'timepicker'  => true,
+        'weeks'       => true,
+        'mask'        => true,
+        'lang'        => 'en',
+        'step'        => 60,    //for time
+        'todayButton' => true,
+        'defaultDate' => false,
+        'defaultTime' => false,
+        'allowTimes'  => [],
+        'allowDates'  => [],
         'attrs' => array(
             'required'    => false,
             'placeholder' => '',
@@ -47,13 +57,25 @@ class plugme_form_control_datetimepicker extends plugme_form_control
             <span class="dashicons dashicons-calendar-alt" style="margin-left:-30px;margin-top:5px;pointer-events: none;"></span><br>
             <script>
                 jQuery(document).ready(function(){
+                    jQuery.datetimepicker.setLocale("'.$this->options['lang'].'");
                     jQuery("#'.$this->attrs_array['id'].'").datetimepicker({ 
                         format: "'.$this->options['format'].'", 
-                        datepicker: '.$this->bool2string($this->options['datepicker']).' 
+                        datepicker: '.$this->bool2String($this->options['datepicker']).', 
+                        timepicker: '.$this->bool2String($this->options['timepicker']).', 
+                        weeks: '.$this->bool2String($this->options['weeks']).',  
+                        mask: '.$this->bool2String($this->options['mask']).',  
+                        step: "'.$this->options['step'].'", 
+                        todayButton: '.$this->bool2String($this->options['todayButton']).',
+                        defaultDate: "'.$this->options['defaultDate'].'", 
+                        defaultTime: "'.$this->options['defaultTime'].'", 
+                        allowTimes: '.$this->array2JsArray($this->options['allowTimes']).', 
+                        allowDates: '.$this->array2JsArray($this->options['allowDates']).',
                     }); 
                 });
             </script>';
 
         return $control;
     }
+
+
 }
